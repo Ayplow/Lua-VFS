a297e5266eb7725fb06ae8d6a7f927877b8ef617
Tom Sputz <tomsputz@outlook.com>
2019-07-05 05:59:41 +0100

    Functional cli
    Still to do:
    Improve code quality accross the board
    Tests
    Static analysis
    Add preload script support (e.g. adding `require` to the environment)

---
+++ src/lua/intercept.lua
@@ -0,0 +1,140 @@
+--[[ TODO
+- Gracefully handle os.exit
+- Fully intercept the io library. E.g io.lines("file")
+]]
+-- iter = io.open("test.lua")
+-- print(load(iter:lines())())
+-- loadfile = nil
+-- io.open = nil
+-- load= nil
+-- os.exit()
+-- print(loadfile "notafile.lku")
+-- loadfile = nil
+
+--[[ Minimum enironvment required for this file:
+_ENV: {
+    rawget: function,
+    error: function,
+    print: function,
+    type: function,
+    loadfile: function, -- Alternatively, a shim can be created with io: { open, stdin: FILE { close, read } } and load
+    _G: <_ENV>
+}
+- _ENV is getfenv() in Lua5.1
+- The functions are all treated as their PUC-Rio counterparts, but no checks are made.
+- Finally, to intercapt them, the script must be able to replace the `io.open` and `loadfile`
+  functions. This will be done with an assignment, meaning use of __newindex is permitted.
+]]
+-- Function signature of Lua5.2 loadfile. Ignores `mode` argument
+local poly_loadfile
+local l_loadfile, l_load, l_print, l_error, l_type, l_io
+    =   loadfile,   load,   print,   error,   type,   io
+local l_open = l_io.open
+
+do
+    local _loadfile = l_loadfile
+    local format = "*a"
+    if not _loadfile then
+        local l_stdin = l_io.stdin
+        local l_read = l_stdin.read
+        local l_close = l_stdin.close
+        if not (l_open and l_read and l_close and l_load) then
+            l_error("Could not polyfill loadfile.")
+        end
+        function _loadfile(filename, ...)
+            local file, err
+            if filename then
+                file, err = l_open(filename)
+            else
+                filename = "stdin"
+                file = l_stdin
+            end
+            if file then
+                local contents = l_read(file, format)
+                l_close(file)
+                return l_load(contents, "=" .. filename, ...)
+            else
+                return nil, "cannot open " .. err
+            end
+        end
+    end
+    if _VERSION == "5.1" then
+        local l_setfenv = setfenv
+        function poly_loadfile(filename, _, env)
+            return l_setfenv(_loadfile(filename), env)
+        end
+    else
+        if _VERSION == "5.3" then
+            format = "a"
+        end
+        poly_loadfile = _loadfile
+    end
+end
+
+local ioop_log = {}
+if l_type(l_open) == "function" then
+    ioop_log.n = 0
+    function io.open(filename, ...)
+        local n     = ioop_log.n + 1
+        ioop_log[n] = filename
+        ioop_log.n  = n 
+        return l_open(filename, ...)
+    end
+end
+local lf_log = {}
+if l_type(l_loadfile) == "function" then
+    lf_log.n = 0
+    function loadfile(filename, ...)
+        local n   = lf_log.n + 1
+        lf_log[n] = filename
+        lf_log.n  = n
+        return l_loadfile(filename, ...)
+    end
+end
+local l_arg = arg
+local init_path = l_arg[0]
+local emuenv_path = ...
+local offset = 1
+local function doinitfile(...)
+    local c_arg = {[-1] = l_arg[-1]}
+    local i, v = 0, init_path
+    repeat
+        c_arg[i] = v
+        i = i + 1
+        v = l_arg[i + offset]
+    until not v
+    arg = c_arg
+    -- TODO: Gracefully handle invalid entrypoint
+    return poly_loadfile(init_path, nil, ...)()
+end
+local function dofiles()
+    if emuenv_path then
+        offset = 2
+        if emuenv_path ~= "-" then
+            arg = {[-1] = l_arg[-1], [0] = emuenv_path}
+            -- TODO: Gracefully handle invalid emuenv
+            return poly_loadfile(emuenv_path)()(doinitfile)
+        end
+    end
+    return doinitfile()
+end
+dofiles()
+
+local list_tab
+function list_tab(tab)
+    if tab.n then
+        for n=1, tab.n do
+            l_print("/ /?/")
+            l_print(tab[n])
+        end
+    end
+end
+l_print("[[VFS::INTERCEPTED]]")
+l_print(l_type(l_load) == "function")
+l_print("/ /:/load log")
+list_tab(lf_log)
+l_print("/ /:/io log")
+list_tab(ioop_log)
+l_print("[[VFS::RESULTS_DONE]]")
+
+while true do end
---
+++ src/lua/scoped_template.lua
@@ -0,0 +1,127 @@
+local __LUA_VFS = {
+    -- TODO: If there is a way to get the runtime CWD, that would be ideal.
+    CWD = {{{cwd}}}, -- type: string - Absolute CWD
+    ENTRYPOINT = {{{entrypoint}}}, -- type: string - Entry lua file path
+    IS_WINDOWS = {{{is_windows}}}, -- type: boolean - Whether the compilation platform was windows
+    DEFAULT = {loadfile = loadfile, io = io, require = require}
+}
+local loadfile, io, require
+__LUA_VFS.FILES = {{{files}}} -- type: Table[string, {string, function}] - Collection of file references keyed by absolute path
+local FILES, CWD = __LUA_VFS.FILES, __LUA_VFS.CWD
+local _loadfile = __LUA_VFS.DEFAULT.loadfile or function(filename)
+    return nil, "cannot open " .. filename .. ": No such file or directory"
+end
+local function assert_arg(n, val, tp, verify, msg, lev)
+    if type(val) ~= tp then
+        error(("argument %d expected a '%s', got a '%s'"):format(n, tp,
+                                                                 type(val)),
+              lev or 2)
+    end
+    if verify and not verify(val) then
+        error(("argument %d: '%s' %s"):format(n, val, msg), lev or 2)
+    end
+end
+local function assert_string(n, val) assert_arg(n, val, 'string', nil, nil, 3) end
+local is_windows = __LUA_VFS.IS_WINDOWS
+local sep = is_windows and '\\' or '/'
+local np_pat1, np_pat2 = ('[^SEP]+SEP%.%.SEP?'):gsub("SEP", sep),
+                         ('SEP+%.?SEP'):gsub("SEP", sep)
+local function normpath(P)
+    assert_string(1, P)
+    if is_windows then
+        if P:match '^\\\\' then -- UNC
+            return '\\\\' .. normpath(P:sub(3))
+        end
+        P = P:gsub('/', '\\')
+    end
+    local k
+    repeat -- /./ -> /
+        P, k = P:gsub(np_pat2, sep)
+    until k == 0
+    repeat -- /A/../ -> /
+        P, k = P:gsub(np_pat1, '')
+    until k == 0
+    if P == '' then P = '.' end
+    return P
+end
+
+local function is_relative(path)
+    return path:sub(1, 1) == '/' or is_windows and path:sub(2, 2) == ':'
+end
+local function abspath(path)
+    return is_relative(path) and path or CWD .. sep .. path
+end
+local function resolve_file(path) return FILES[normpath(abspath(path))] end
+-- TODO: Remove this function
+local function TODO() error "Not Implemented" end
+local filemeta = {
+    read = function(handle, format)
+        local contents = handle[2][1]
+        if format == "a" then
+            local current = handle[1]
+            handle[1] = #contents
+            return contents:sub(current, handle[1])
+        elseif format == "l" then
+            local current = handle[1]
+            local to = contents:find("\n", current)
+            handle[1] = to + 1
+            return contents:sub(current, to - 1)
+        end
+        return TODO()
+    end,
+    write = TODO,
+    seek = TODO,
+    setvbuf = TODO,
+    lines = TODO,
+    flush = TODO,
+    close = function(handle)
+        -- TODO: Fully implement
+    end,
+    __tostring = TODO,
+    __gc = TODO,
+    __name = "FILE*"
+}
+filemeta.__index = filemeta
+local function file(ref, mode) return setmetatable({0, ref}, filemeta) end
+io = setmetatable({
+    open = function(filename, mode)
+        local ref = resolve_file(filename)
+        if ref then return file(ref, mode) end
+        -- TODO: Implement proper error handling
+        return nil, filename .. ": No such file or directory", 2
+    end
+}, {__index = __LUA_VFS.DEFAULT.io})
+local bound_tmpl = "loadfile, io = ... return function(...)\n%s\nend"
+local format = string.format
+loadfile = load and function(...)
+    local filename, mode, env = ...
+    local file, err
+    if filename then
+        file, err = io.open(filename)
+    else
+        filename = "stdin"
+        file = io.stdin
+    end
+    if file then
+        -- TODO: Check if this binding template is really doing anything useful.
+        return load(format(bound_tmpl, file:read("a")), "=" .. filename, mode,
+                    env or _ENV)(loadfile, io)
+    else
+        return _loadfile()
+    end
+end or function(...)
+    local filename, mode, env = ...
+    -- If we don't have load, fall back to using preloaded functions
+    local file = resolve_file(filename)
+    if file then
+        return file[2](env or _ENV)
+    else
+        return _loadfile(...)
+    end
+end
+
+-- Patches for enviroments that lack the standard library
+arg = arg or {[-1] = "lua53.exe", [0] = "init.lua"}
+os = os or {}
+os.getenv = os.getenv or function() end
+return loadfile(__LUA_VFS.ENTRYPOINT)(...)
