f0aefcc5dad7dd91cf0425545787045d4d44092a
Tom Sputz <tomsputz@outlook.com>
2019-11-12 07:15:02 +0000

    More useful errors in testing

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,29 +1,37 @@
+use assert_cmd::prelude::*;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+use tempfile::NamedTempFile;
+use failure::ResultExt;
 
-#[cfg(test)]
-mod integration {
-    use std::path::Path;
-    use std::process::Command;
-    use assert_cmd::prelude::*;
-
-
-    fn test_bundle(path: &str) {
-        let output = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
-          .current_dir(format!("tests/{}", path))
-          .arg("--preload")
-          .output().unwrap();
-        assert_eq!(
-        String::from_utf8(Command::new("lua")
-          .arg("-e").arg("loadfile = nil load = nil io = nil")
-          .arg(format!("tests/{}/init.bundle.lua", path))
-          .output().unwrap().stdout).unwrap(),
-        String::from_utf8(Command::new("lua")
-          .current_dir(format!("tests/{}", path))
-          .arg("init.lua")
-          .output().unwrap().stdout).unwrap());
-        std::fs::remove_file(format!("tests/{}/init.bundle.lua", path));
-    }
-    #[test]
-    fn external_txt() {
-        test_bundle("external_txt")
-    }
+fn test_bundle(path: &Path) -> Result<(), exitfailure::ExitFailure> {
+    let mut bundle = NamedTempFile::new()?;
+    bundle.write_all(
+        &Command::cargo_bin(env!("CARGO_PKG_NAME"))?
+            .current_dir(path)
+            .arg("--preload")
+            .output()?
+            .stdout,
+    )?;
+    // TODO: Improve usability for alternative locations
+    which::which("lua").context("lua interpreter not present in PATH")?;
+    Ok(assert_eq!(
+        Command::new("lua")
+            .arg("-e")
+            .arg("loadfile = nil load = nil io = nil")
+            .arg(bundle.path())
+            .output()?
+            .stdout,
+        Command::new("lua")
+            .current_dir(path)
+            .arg("init.lua")
+            .output()?
+            .stdout
+    ))
+}
+#[test]
+fn external_txt() -> Result<(), exitfailure::ExitFailure> {
+    // TODO: Can we improve the way this folder is located?
+    test_bundle(Path::new("tests/external_txt"))
 }
